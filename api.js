30af8eb94ccdec6a9901ec71b163527db08ec5d4
Rushikesh Jogadand <jogadand4@gmail.com>
2024-07-04 02:02:29 +0530

    git

---
+++ api.js
@@ -0,0 +1,17 @@
+//   const express = require('express');
+//         const app = express();
+//         const cors = require('cors');
+
+//         app.use(cors());
+
+//         app.get('manifest.json', (req, res) => {
+//             res.json({
+//                 "url": "https://your-app-url/tonconnect-manifest.json",
+//                 "name": "TON Vote",
+//                 "iconUrl": "https://ton.vote/logo.png"
+//             });
+//         });
+
+//         app.listen(3000, () => {
+//             console.log('Server is running on port 3000');
+//         });
